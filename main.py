3108baf1109d21a5be1b5333fc5aab99f277a700
jmullercuber <jmullercuber@gmail.com>
2022-01-26 03:52:03 -0700

    mvp

---
+++ main.py
@@ -0,0 +1,61 @@
+import datetime
+import os
+import time
+
+import cv2
+import boto3
+
+CAMERA_PORT = 0
+CAMERA_LOAD_TIME_SECS = 3.0
+LOCAL_STORAGE_DIR = "data"
+BUCKET_NAME = os.environ['BUCKET_NAME']
+
+def get_image():
+    camera = cv2.VideoCapture(CAMERA_PORT)
+    time.sleep(CAMERA_LOAD_TIME_SECS)
+    ret, img = camera.read()
+    del(camera)  # free camera ASAP
+    
+    # no errors
+    if ret:
+        return img
+
+def generate_file_name():
+    iso_now = datetime.datetime.now().isoformat()
+    file_name = f"{LOCAL_STORAGE_DIR}/{iso_now}.jpg"
+    return file_name
+
+def save_image_locally(img, file_name):
+    cv2.imwrite(file_name, img)
+    return file_name
+
+def upload(local_file_name, s3_key=None):
+    # key default value
+    if not s3_key:
+        s3_key = local_file_name
+
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME)
+    print(bucket.name)
+
+    bucket.upload_file(local_file_name, local_file_name)
+
+def main():
+    img = get_image()
+    if img:
+        print("Captured image")
+    else:
+        print("Failed to get image T_T")
+        return
+
+    local_file_name = generate_file_name()
+    print(f"Generated File Name: {local_file_name}")
+
+    save_image_locally(img, local_file_name)
+    print("Successfully saved image locally")
+
+    upload(local_file_name)
+    print(f"Upload of {local_file_name} done")
+
+if __name__ == "__main__":
+    main()
